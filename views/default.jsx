bdeb9759323289fe98ad5f008b9b01f8e54ca16b
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-02-23 11:53:42 -0500

    added sticky footer from bonus

--- views/default.jsx
+++ views/default.jsx
@@ -9,7 +9,11 @@ function Def(html) {
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
 			</head>
 			<body>
-				{html.children}
+			<div id="wrapper">
+               <div className="main-content">{html.children}</div>
+               <footer className="footer"><a href="https://www.linkedin.com/in/kristen-von-gerichten-80aba6233" target="blank" rel="noopener noreferrer">Connect with me on LinkedIn</a></footer>
+				{/* {html.children} */}
+			</div>	
 			</body>
 		</html>
 	)
